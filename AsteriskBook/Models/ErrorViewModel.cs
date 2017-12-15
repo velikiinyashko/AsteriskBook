3cba2e11acfa4e9fa120c4f078f28c6e334c05da
Ivanov Sergey <admin@sibitproject.ru>
2017-12-15 17:56:38 +0700

    update

---
+++ AsteriskBook/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AsteriskBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
